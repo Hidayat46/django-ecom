a2ef6fba091bd71d153ba23583931ada4b619983
hidayat <hidayatyt46@gmail.com>
2024-12-29 23:59:38 +0500

    new changes

--- store/views.py
+++ store/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("hello world")
+    return HttpResponse("hello world inshallah i'll prove them wrong those people who think about me ")
